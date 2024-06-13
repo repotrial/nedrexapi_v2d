f2d3c64ad70c65fdbb9538074f2ea756e35d6cd5
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-06-13 19:38:27 +0200

    PPI route update (#7)
    * security update for dependencies
    
    * testing removal of / base in addition ot api.base
    
    * fixed dependency sceurity issue of requests
    
    * add reviewed_proteins param to graph builder (#3)
    
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>
    
    * changed mondo_id_Mapping from get to post
    
    * fixed comorbiditome api
    
    * Small fixes and improvements (#4) (#5)
    
    * security update for dependencies
    
    * testing removal of / base in addition ot api.base
    
    * fixed dependency sceurity issue of requests
    
    * add reviewed_proteins param to graph builder (#3)
    
    
    
    * changed mondo_id_Mapping from get to post
    
    * fixed comorbiditome api
    
    ---------
    
    Co-authored-by: ahoewi <112104849+ahoewi@users.noreply.github.com>
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>
    
    * Add reviewed_proteins param to pagnated ppi query (#6)
    
    * add reviewed_proteins param to graph builder
    
    * integrate reviewed_proteins param to pagnated ppi query
    
    ---------
    
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>
    
    * added ppi route condition for all proteins
    
    * trying to make output deterministic and sorted
    
    * reverting bool to string conversion
    
    * fixing filtered_protein set issue
    
    * trying to fix reviewed filter issue
    
    * converted to propper post request
    
    ---------
    
    Co-authored-by: ahoewi <112104849+ahoewi@users.noreply.github.com>
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>

--- nedrexapi/routers/ppi.py
+++ nedrexapi/routers/ppi.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter as _APIRouter
 from fastapi import HTTPException as _HTTPException
 from fastapi import Query as _Query
+from pydantic import BaseModel as _BaseModel
+from pydantic import Field as _Field
 
 from nedrexapi.common import _API_KEY_HEADER_ARG, check_api_key_decorator
 from nedrexapi.config import config as _config
@@ -10,33 +12,58 @@ DEFAULT_QUERY = _Query(None)
 
 router = _APIRouter()
 
+class PPIRequest(_BaseModel):
+    nodes: list[str] = _Field(None, title="Primary domain IDs of nodes",
+                              description="Primary domain IDs of the nodes the attributes are requested for")
+    iid_evidence: list[str] = _Field(['exp'], title="Evidence types",description="The evidence types to filter the PPIs by")
+    skip: int = _Field(0, title="Skip", description="The number of PPIs to skip")
+    limit: int = _Field(10000, title="Limit", description="The number of PPIs to return")
+    reviewed_proteins: list[bool] = _Field([True,False], title="Reviewed proteins", description="Whether to filter by reviewed proteins")
+    skip_proteins: int = _Field(0, title="Skip proteins", description="The number of proteins to skip")
+    limit_proteins: int = _Field(250000, title="Limit proteins", description="The number of proteins to return")
+   
 
-@router.get("/ppi", summary="Paginated PPI query")
+    class Config:
+        extra = "forbid"
+
+
+_DEFAULT_PPI_REQUEST = PPIRequest()
+
+
+@router.post("/ppi", summary="Paginated PPI query")
 @check_api_key_decorator
 def get_paginated_protein_protein_interactions(
-    iid_evidence: list[str] = DEFAULT_QUERY,
-    skip: int = DEFAULT_QUERY,
-    limit: int = DEFAULT_QUERY,
+    ppi_request: PPIRequest = _DEFAULT_PPI_REQUEST,
     x_api_key: str = _API_KEY_HEADER_ARG,
 ):
     """
     Returns an array of protein protein interactions (PPIs in a paginated manner). A skip and a limit can be
     specified, defaulting to `0` and `10_000`, respectively, if not specified.
     """
-    if not iid_evidence:
+    if not ppi_request.iid_evidence:
         return []
 
-    if not skip:
-        skip = 0
-    if not limit:
-        limit = _config["api.pagination_max"]
-    elif limit > _config["api.pagination_max"]:
-        raise _HTTPException(status_code=422, detail=f"Limit specified ({limit}) greater than maximum limit allowed")
+    if not ppi_request.skip:
+        ppi_request.skip = 0
+    if not ppi_request.limit:
+        ppi_request.limit = _config["api.pagination_max"]
+    elif ppi_request.limit > _config["api.pagination_max"]:
+        raise _HTTPException(status_code=422, detail=f"Limit specified ({ppi_request.limit}) greater than maximum limit allowed")
+    
+    query = {"evidenceTypes": {"$in": ppi_request.iid_evidence}}
+    if not (True in ppi_request.reviewed_proteins and False in ppi_request.reviewed_proteins) or ppi_request.skip_proteins > 0 or ppi_request.limit_proteins < 250000:
+        protein_query = {"is_reviewed": {"$in": [str(r) for r in ppi_request.reviewed_proteins]}}
+    
+        filtered_proteins = list({
+            protein["primaryDomainId"]
+            for protein in MongoInstance.DB()["protein"].find(protein_query).sort('_id').skip(ppi_request.skip_proteins).limit(ppi_request.limit_proteins)
+        })
+        query.update({"memberOne": {"$in": filtered_proteins}, "memberTwo": {"$in": filtered_proteins}})
 
-    query = {"evidenceTypes": {"$in": iid_evidence}}
     coll_name = "protein_interacts_with_protein"
 
     return [
         {k: v for k, v in doc.items() if k != "_id"}
-        for doc in MongoInstance.DB()[coll_name].find(query).skip(skip).limit(limit)
+        # each entry is one document -> finds all documents by conditions
+        for doc in MongoInstance.DB()[coll_name].find(query).sort('_id').skip(ppi_request.skip).limit(ppi_request.limit)
     ]
