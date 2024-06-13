a870d41b2f3772ba5253a87d9508456da394a2f1
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-06-13 18:30:41 +0200

    reverting bool to string conversion

--- nedrexapi/routers/ppi.py
+++ nedrexapi/routers/ppi.py
@@ -17,7 +17,7 @@ def get_paginated_protein_protein_interactions(
     iid_evidence: list[str] = ['exp'],
     skip: int = 0,
     limit: int = 10000,
-    reviewed_proteins: list[bool] = [True, False],
+    reviewed_proteins: list[bool] = ["True", "False"],
     skip_proteins: int = 0,
     limit_proteins: int = 250000,
     x_api_key: str = _API_KEY_HEADER_ARG,
@@ -37,8 +37,8 @@ def get_paginated_protein_protein_interactions(
         raise _HTTPException(status_code=422, detail=f"Limit specified ({limit}) greater than maximum limit allowed")
     
     query = {"evidenceTypes": {"$in": iid_evidence}}
-    if not (True in reviewed_proteins and False in reviewed_proteins):
-        protein_query = {"is_reviewed": {"$in": [str(r) for r in reviewed_proteins]}}
+    if not ("True" in reviewed_proteins and "False" in reviewed_proteins):
+        protein_query = {"is_reviewed": {"$in": reviewed_proteins}}
     
         filtered_proteins = {
             protein["primaryDomainId"]
