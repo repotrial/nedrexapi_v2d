f272a9a24d57d206a26912eaaf222c696e08a249
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-09-06 13:43:45 +0200

    added stream boolean for neo4j queries

--- nedrexapi/routers/neo4j.py
+++ nedrexapi/routers/neo4j.py
@@ -1,7 +1,7 @@
 import json
 
 from fastapi import APIRouter as _APIRouter
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 from more_itertools import chunked
 from py2neo import Graph  # type: ignore
 
@@ -14,15 +14,19 @@ _NEO4J_DRIVER = Graph(f"bolt://{_NEO4J_HOST}:{_NEO4J_PORT}")
 router = _APIRouter()
 
 
-async def run_query(query):
-    print(query)
+async def run_query_stream(query):
     result = _NEO4J_DRIVER.run(query)
     for chunk in chunked(result, 1_000):
         yield json.dumps([json.loads(json.dumps(i, default=lambda o: dict(o))) for i in chunk]) + "\n"
 
 
+def run_query(query):
+    result = _NEO4J_DRIVER.run(query)
+    return json.dumps([json.loads(json.dumps(i, default=lambda o: dict(o))) for i in result])
+
+
 @router.get("/query", summary="Neo4j query")
-def neo4j_query(query: str):
+def neo4j_query(query: str, stream: bool = True):
     """
     Runs a Neo4j query and returns the result.
     The result is returned as a streaming response, so it is up to the user to handle the streaming response.
@@ -32,8 +36,35 @@ def neo4j_query(query: str):
         import requests
         query = "MATCH (n) RETURN n LIMIT 25"
         url = "http://82.148.225.92:8022/neo4j/query"
-        response = requests.get(url, params={"query":query}, stream=True)
+        response = requests.get(url, params={"query":query, "stream":True}, stream=True)
         for line in response.iter_lines():
             print(json.loads(line.decode()))
     """
-    return StreamingResponse(run_query(query))
+    if stream:
+        return StreamingResponse(run_query_stream(query))
+    else:
+        return Response(run_query(query))
+
+from pydantic import BaseModel as _BaseModel
+class QueryRequest(_BaseModel):
+    query: str
+
+@router.post("/query", summary="Neo4j query")
+def neo4j_query(qr: QueryRequest):
+    """
+    Runs a Neo4j query and returns the result.
+    The result is returned as a streaming response, so it is up to the user to handle the streaming response.
+    An example of this using Python's requests library is below:
+
+        import json
+        import requests
+        query = "MATCH (n) RETURN n LIMIT 25"
+        url = "https://api.nedrex.net/neo4j/query"
+        response = requests.post(url, json={"query":query, "stream":False})
+        for line in response.iter_lines():
+            print(json.loads(line.decode()))
+    """
+    if qr.stream:
+        return StreamingResponse(run_query_stream(qr.query))
+    else:
+        return Response(run_query(qr.query))
