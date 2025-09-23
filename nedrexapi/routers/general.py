f9901e895196aaad871ab0131e1ad1b05c6b9ade
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-09-23 16:21:15 +0200

    adding entrez to uniprot ids route

--- nedrexapi/routers/general.py
+++ nedrexapi/routers/general.py
@@ -577,17 +577,17 @@ def id_map(collection: str, q: list[str] = DEFAULT_QUERY, x_api_key: str = _API_
     result = {item: get_primary_id(item, collection) for item in q}
     return result
 
-@router.get("/entrez_to_uniprot",
+@router.get("/entrez_to_uniprot_id",
     responses = {
         200: {"content": {"application/json": { "entrez.1234":["uniprot.3213", "uniprot.4321"]}}},
         404: {"content": {"application/json": {"1234": "Not a valid entrez ID"}}}
     },
-    summary = "Translates entrez gene IDs to UniProt protein IDs",
+    summary = "Translate single entrez to UniProt IDs",
             )
 @check_api_key_decorator
 def translate_entrez_to_uniprot(q: str, x_api_key: str = _API_KEY_HEADER_ARG):
     """
-    Returns a mapping of `{user-supplied-id: [primaryDomainId]}` for a user-specified identifier in a
+    Returns a mapping of `{user-supplied-id: [primaryDomainId]}` in
     entrez space to all mapped versions of uniprot IDs.
     The values in the hash map are a list, as multiple protein IDs can exist of a single entrez ID.
     """
@@ -608,8 +608,40 @@ def translate_entrez_to_uniprot(q: str, x_api_key: str = _API_KEY_HEADER_ARG):
 
     return response
 
+@router.post("/entrez_to_uniprot_ids",
+    responses = {
+        200: {"content": {"application/json": { "entrez.1234":["uniprot.3213", "uniprot.4321"], "entrez.4341":["uniprot:7265"]}}},
+        404: {"content": {"application/json": {"1234": "Not a valid entrez ID"}}}
+    },
+    summary = "Translate multiple entrez to UniProt IDs",
+            )
+@check_api_key_decorator
+def translate_entrez_to_uniprot(idr: IDRequest, x_api_key: str = _API_KEY_HEADER_ARG):
+    """
+    Returns a mapping of multiple `{user-supplied-id: [primaryDomainId]}` in
+    entrez space to all mapped versions of uniprot IDs.
+    The values in the hash map are a list, as multiple protein IDs can exist of a single entrez ID.
+    """
+    if not idr.id_list:
+        return {}
 
+    if len(idr.id_list) == 0:
+        return {}
 
+    response = dict()
+    query_set = set()
+
+    for q in idr.id_list:
+        response[q] = set()
+        if not q.startswith("entrez."):
+            response[q] = "Not a valid entrez ID"
+        else:
+            query_set.add(q)
+
+    for entry in MongoInstance.DB()["protein_encoded_by_gene"].find({"targetDomainId": {"$in" :query_set}}):
+        response[entry["targetDomainId"]].add(entry["sourceDomainId"])
+
+    return response
 
 
 
