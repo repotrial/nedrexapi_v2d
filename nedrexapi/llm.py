dca44fbd963bc2a0017ed6e7db790db6f00b2871
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-01-15 11:07:42 +0100

    debugging chat auth issue

--- nedrexapi/llm.py
+++ nedrexapi/llm.py
@@ -10,18 +10,22 @@ _LLM_path=_config[f"embeddings.path"]
 # _LLM_user=_config[f"embeddings.user"]
 # _LLM_pass=_config[f"embeddings.pass"]
 
-_LLM_chat_model=_config[f"chat.model"]
-_LLM_chat_base=_config[f"chat.server_base"]
+_LLM_chat_model=_config["chat.model"]
+_LLM_chat_base=_config["chat.server_base"]
+_LLM_chat_api_key=_config["chat.api_key"]
+
+headers = {"Authorization": "Bearer " + _LLM_chat_api_key}
 
 
 def get_embedder():
-    return OllamaEmbeddings(base_url=_LLM_BASE, model=_LLM_model)
+    return OllamaEmbeddings(base_url=_LLM_BASE, model=_LLM_model, headers=headers)
 
 def get_generator():
-    return Ollama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0)
+    return Ollama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0, headers=headers)
 
 def get_chat():
-    return ChatOllama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0)
+    print(headers)
+    return ChatOllama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0, client_kwargs={'headers': headers})
 
 def get_embedding(query):
     embedder = get_embedder()
@@ -31,7 +35,7 @@ def get_embedding(query):
 
 def generate(query):
     ollama_llm = get_generator()
-    response = ollama_llm.invoke(input=query)
+    response = ollama_llm.invoke(query)
     return response
 
 
