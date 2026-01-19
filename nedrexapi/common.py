215b2828977dab24d8b3313e3703344201179b0c
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-01-19 18:52:40 +0100

    reformatting code that might have caused issues

--- nedrexapi/common.py
+++ nedrexapi/common.py
@@ -216,13 +216,5 @@ def check_api_key_decorator(func):
 
 _API_KEY_HEADER_ARG = _Header(default=None, include_in_schema=_config["api.require_api_keys"])
 
-NODE_COLLECTIONS = [
-    node_coll
-    for node_coll in _config["api.node_collections"]
-    if node_coll in MongoInstance.DB().list_collection_names()
-]
-EDGE_COLLECTIONS = [
-    edge_coll
-    for edge_coll in _config["api.edge_collections"]
-    if edge_coll in MongoInstance.DB().list_collection_names()
-]
+NODE_COLLECTIONS = [node_coll for node_coll in _config["api.node_collections"] if node_coll in MongoInstance.DB().list_collection_names() ]
+EDGE_COLLECTIONS = [ edge_coll for edge_coll in _config["api.edge_collections"] if edge_coll in MongoInstance.DB().list_collection_names() ]
