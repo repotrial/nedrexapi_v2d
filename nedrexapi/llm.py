123736edd61ca8ca62a5c8c2b4b2f43e40efd2f7
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-03-13 15:17:47 +0100

    dependency udpates and small adjustements handling misconfigured embedding or chatting options

--- nedrexapi/llm.py
+++ nedrexapi/llm.py
@@ -3,18 +3,23 @@ from langchain_ollama.chat_models import ChatOllama
 from nedrexapi.config import config as _config
 from langchain_community.llms.ollama import Ollama
 
-_LLM_BASE=_config["embeddings.server_base"]
-_LLM_model=_config[f"embeddings.model"]
-_LLM_path=_config[f"embeddings.path"]
+def get_conf_or_none(key):
+    try:
+        return _config.get(key)
+    except:
+        return None
 
-# _LLM_user=_config[f"embeddings.user"]
-# _LLM_pass=_config[f"embeddings.pass"]
+_LLM_BASE=get_conf_or_none("embeddings.server_base")
+_LLM_model=get_conf_or_none("embeddings.model")
+_LLM_path=get_conf_or_none("embeddings.path")
 
-_LLM_chat_model=_config["chat.model"]
-_LLM_chat_base=_config["chat.server_base"]
-_LLM_chat_api_key=_config["chat.api_key"]
+_LLM_chat_model=get_conf_or_none("chat.model")
+_LLM_chat_base=get_conf_or_none("chat.server_base")
+_LLM_chat_api_key=get_conf_or_none("chat.api_key")
 
-headers = {"Authorization": "Bearer " + _LLM_chat_api_key}
+headers = {"Authorization": "Bearer " + str(_LLM_chat_api_key)}
+
+
 
 
 def get_embedder():
@@ -24,14 +29,11 @@ def get_generator():
     return Ollama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0, headers=headers)
 
 def get_chat():
-    print(headers)
     return ChatOllama(base_url=_LLM_chat_base, model=_LLM_chat_model, temperature=0.0, client_kwargs={'headers': headers})
 
 def get_embedding(query):
     embedder = get_embedder()
     return embedder.embed(query)
-
-
 
 def generate(query):
     ollama_llm = get_generator()
